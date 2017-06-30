0590606c58189aac89a91f4a9b49c76193019a28
Nesnomis90 <Jusimonsen@gmail.com>
2017-06-30 13:55:10 +0200

    tiny tweaks

--- admin/events/addevent.html.php
+++ admin/events/addevent.html.php
@@ -8,108 +8,112 @@
 		<title>Schedule A New Event</title>
 	</head>
 	<body>
-		<h1>Schedule A New Event</h1>
-		
-		<div class="warning">
-		<?php if(isset($_SESSION['AddEventError'])) : ?>
-			<b><?php htmlout($_SESSION['AddEventError']); ?></b>
-			<?php unset($_SESSION['AddEventError']); ?>
-		<?php endif; ?>
-		</div>
-<!--- TO-DO: Fix this. This is a copypaste mess right now --->
-		<form action="" method="post">
-			<div>
-				<label for="checkboxMeetingroom">Select the meeting room(s) the event should appear in: </label>
+		<fieldset><legend><b>Schedule A New Event</b></legend>
+			<div class="warning">
+			<?php if(isset($_SESSION['AddEventError'])) : ?>
+				<b><?php htmlout($_SESSION['AddEventError']); ?></b>
+				<?php unset($_SESSION['AddEventError']); ?>
+			<?php endif; ?>
 			</div>
 			
-			<div>
-				<input type="checkbox" name="meetingroomAll" value="All" <?php htmlout($checkAll); ?>>All<br />
-				<?php foreach($checkboxes AS $checkbox) : ?>
-					<?php //checkbox[0] is the meeting room ID ?>
-					<?php //checkbox[1] is the meeting room name ?>
-					<?php //checkbox[2] is if it should have a linefeed ?>
-					<?php //checkbox[3] is if it should be checked ?>
-					<?php if($checkbox[3]) : ?>
-						<input type="checkbox" name="meetingroom[]" 
-						value="<?php htmlout($checkbox[0]); ?>" checked="checked"><?php htmlout($checkbox[1]); ?>
-					<?php else : ?>
-						<input type="checkbox" name="meetingroom[]" 
-						value="<?php htmlout($checkbox[0]); ?>"><?php htmlout($checkbox[1]); ?>
-					<?php endif; ?>
-					<?php if($checkbox[2]): ?><br /><?php endif; ?>
-				<?php endforeach; ?>
-			<div>
-			
-			<div>
-				<label for="meetingRoomID">Meeting Room: </label>
-				<select name="meetingRoomID" id="meetingRoomID">
-					<?php foreach($meetingroom as $row): ?> 
-						<?php if($row['meetingRoomID']==$selectedMeetingRoomID):?>
-							<option selected="selected" value="<?php htmlout($row['meetingRoomID']); ?>"><?php htmlout($row['meetingRoomName']);?></option>
+			<form action="" method="post">
+				<div>
+					<label for="checkboxMeetingroom">Select the meeting room(s) the event should appear in: </label>
+				</div>
+				
+				<div>
+					<input type="checkbox" name="meetingroomAll" value="All" <?php htmlout($checkAll); ?>>All<br />
+					<?php foreach($checkboxes AS $checkbox) : ?>
+						<?php //checkbox[0] is the meeting room ID ?>
+						<?php //checkbox[1] is the meeting room name ?>
+						<?php //checkbox[2] is if it should have a linefeed ?>
+						<?php //checkbox[3] is if it should be checked ?>
+						<?php if($checkbox[3]) : ?>
+							<input type="checkbox" name="meetingroom[]" 
+							value="<?php htmlout($checkbox[0]); ?>" checked="checked"><?php htmlout($checkbox[1]); ?>
 						<?php else : ?>
-							<option value="<?php htmlout($row['meetingRoomID']); ?>"><?php htmlout($row['meetingRoomName']);?></option>
-						<?php endif;?>
+							<input type="checkbox" name="meetingroom[]" 
+							value="<?php htmlout($checkbox[0]); ?>"><?php htmlout($checkbox[1]); ?>
+						<?php endif; ?>
+						<?php if($checkbox[2]): ?><br /><?php endif; ?>
 					<?php endforeach; ?>
-				</select>				
-			</div>
-			
-			<div>
-				<label for="startTime">Start Time: </label>
-				<input type="text" name="startTime" id="startTime" 
-				placeholder="hh:mm:ss"
-				value="<?php htmlout($startTime); ?>">
-			</div>
-			
-			<div>
-				<label for="endTime">End Time: </label>
-				<input type="text" name="endTime" id="endTime" 
-				placeholder="hh:mm:ss"
-				value="<?php htmlout($endTime); ?>">
-			</div>
+				<div>
+				
+				<div>
+					<label for="meetingRoomID">Meeting Room: </label>
+					<select name="meetingRoomID" id="meetingRoomID">
+						<?php foreach($meetingroom as $row): ?> 
+							<?php if($row['meetingRoomID']==$selectedMeetingRoomID):?>
+								<option selected="selected" value="<?php htmlout($row['meetingRoomID']); ?>"><?php htmlout($row['meetingRoomName']);?></option>
+							<?php else : ?>
+								<option value="<?php htmlout($row['meetingRoomID']); ?>"><?php htmlout($row['meetingRoomName']);?></option>
+							<?php endif;?>
+						<?php endforeach; ?>
+					</select>				
+				</div>
+				
+				<div>
+					<label for="startTime">Start Time: </label>
+					<input type="text" name="startTime" id="startTime" 
+					placeholder="hh:mm:ss"
+					value="<?php htmlout($startTime); ?>">
+				</div>
+				
+				<div>
+					<label for="endTime">End Time: </label>
+					<input type="text" name="endTime" id="endTime" 
+					placeholder="hh:mm:ss"
+					value="<?php htmlout($endTime); ?>">
+				</div>
 
-			<div>
-				<label for="eventName">Event Name: </label>
-				<input type="text" name="eventName" id="eventName" 
-				value="<?php htmlout($eventName); ?>">
-			</div>
-			
-			<div>
-				<label class="description" for="eventDescription">Event Description: </label>
-				<textarea rows="4" cols="50" name="eventDescription" id="eventDescription"><?php htmlout($eventDescription); ?></textarea>
-			</div>
-			
-			<div>
-				<fieldset><legend>Select the day(s) for the event</legend>
-					<input type="checkbox" name="daysSelected[]" value="Monday">Monday<br />
-					<input type="checkbox" name="daysSelected[]" value="Tuesday">Tuesday<br />
-					<input type="checkbox" name="daysSelected[]" value="Wednesday">Wednesday<br />
-					<input type="checkbox" name="daysSelected[]" value="Thursday">Thursday<br />
-					<input type="checkbox" name="daysSelected[]" value="Friday">Friday<br />
-					<input type="checkbox" name="daysSelected[]" value="Saturday">Saturday<br />
-					<input type="checkbox" name="daysSelected[]" value="Sunday">Sunday
-					<input class="bottomRight" type="submit" name="add" value="Confirm Days">
-				</fieldset>
-			</div>
-			
-			<div>
-				<label for="">Select if event should repeat: </label>
-				<input type="submit" name="add" value="Repeat">
-			</div>
-			
-			<div>
-				<input type="submit" name="add" value="Reset">
-				<input type="submit" name="add" value="Cancel">
-				<?php if(isset($_SESSION['AddEventChangeDays']) AND $_SESSION['AddEventChangeDays']) : ?>
-					<input type="submit" name="disabled" value="Create Event" disabled>
-					<b>You need to select the day(s) you want before you can create the event.</b>
-				<?php elseif(!isset($_SESSION['AddEventSelectedDays'])) : ?>
-					<input type="submit" name="disabled" value="Create Event" disabled>
-					<b>You need to select at least one day you want before you can create the event.</b>
-				<?php else : ?>
-					<input type="submit" name="add" value="Create Event">
-				<?php endif; ?>				
-			</div>
-		</form>
+				<div>
+					<label for="eventName">Event Name: </label>
+					<input type="text" name="eventName" id="eventName" 
+					value="<?php htmlout($eventName); ?>">
+				</div>
+				
+				<div>
+					<label class="description" for="eventDescription">Event Description: </label>
+					<textarea rows="4" cols="50" name="eventDescription" id="eventDescription"><?php htmlout($eventDescription); ?></textarea>
+				</div>
+				
+				<div>
+					<fieldset><legend>Select the day(s) for the event</legend>
+						<input type="checkbox" name="daysSelected[]" value="Monday">Monday<br />
+						<input type="checkbox" name="daysSelected[]" value="Tuesday">Tuesday<br />
+						<input type="checkbox" name="daysSelected[]" value="Wednesday">Wednesday<br />
+						<input type="checkbox" name="daysSelected[]" value="Thursday">Thursday<br />
+						<input type="checkbox" name="daysSelected[]" value="Friday">Friday<br />
+						<input type="checkbox" name="daysSelected[]" value="Saturday">Saturday<br />
+						<input type="checkbox" name="daysSelected[]" value="Sunday">Sunday
+						<?php if(isset($_SESSION['AddEventDaysSelected']) AND $_SESSION['AddEventDaysSelected']) : ?>
+							<input class="bottomRight" type="submit" name="add" value="Change Days">
+						<?php else : ?>
+							<input class="bottomRight" type="submit" name="add" value="Confirm Days">
+						<?php endif; ?>
+					</fieldset>
+				</div>
+				
+				<div>
+					<label for="">Select if event should repeat: </label>
+					<input type="submit" name="add" value="Repeat">
+				</div>
+				
+				<div>
+					<input type="submit" name="add" value="Reset">
+					<input type="submit" name="add" value="Cancel">
+					<?php if(!isset($_SESSION['AddEventDaysSelected'])) : ?>
+						<input type="submit" name="disabled" value="Create Event" disabled>
+						<b>You need to select the day(s) you want before you can create the event.</b>
+					<?php elseif(isset($_SESSION['AddEventDaysSelected']) AND $_SESSION['AddEventDaysSelected'] == 0) : ?>
+						<input type="submit" name="disabled" value="Create Event" disabled>
+						<b>You need to select at least one day you want before you can create the event.</b>
+					<?php else : ?>
+						<input type="submit" name="add" value="Create Event">
+					<?php endif; ?>				
+				</div>
+			</form>
+		</fieldset>
 	<p><a href="..">Return to CMS home</a></p>
 	<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/logout.inc.html.php'; ?>
 	</body>
