04da6c2890363269d90dbab3232598832ffbdc1d
Nesnomis90 <Jusimonsen@gmail.com>
2017-07-13 14:21:24 +0200

    Working on adding user info functionalities

--- user/user.html.php
+++ user/user.html.php
@@ -6,18 +6,28 @@
 	<head>
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="/CSS/myCSS.css">
-		<script src="/scripts/myFunctions.js"></script>			
-		<title>User Information</title>
+		<script src="/scripts/myFunctions.js"></script>	
+		<?php if(isset($_SESSION['loggedIn']) : ?>
+			<title>Your User Information</title>
+		<?php else : ?>
+			<title>User Information</title>
+		<?php endif; ?>
 	</head>
 	<body>
 		<?php include_once $_SERVER['DOCUMENT_ROOT'] .'/includes/topnav.html.php'; ?>
 		
-		<h1>User Information</h1>
-		
-		<?php if(isset($_SESSION['normalUserFeedback'])) : ?>
-			<span><b class="feedback"><?php htmlout($_SESSION['normalUserFeedback']); ?></b></span>
-			<?php unset($_SESSION['normalUserFeedback']); ?>
+		<?php if(isset($_SESSION['loggedIn']) : ?>
+			<h1>Your User Information</h1>
+		<?php else : ?>
+			<h1>User Information</h1>
 		<?php endif; ?>
+		
+		<div class="left">
+			<?php if(isset($_SESSION['normalUserFeedback'])) : ?>
+				<span><b class="feedback"><?php htmlout($_SESSION['normalUserFeedback']); ?></b></span>
+				<?php unset($_SESSION['normalUserFeedback']); ?>
+			<?php endif; ?>
+		</div>
 
 		<?php if(isset($_SESSION['loggedIn']) : ?>
 			
