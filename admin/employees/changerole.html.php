f0e52db92a19d16e15eb57ce61040d3b8c9e0dda
Nesnomis90 <Jusimonsen@gmail.com>
2017-08-02 13:35:32 +0200

    Adding fieldsets to employee templates

--- admin/employees/changerole.html.php
+++ admin/employees/changerole.html.php
@@ -16,44 +16,45 @@
 	<body onload="startTime()">
 		<?php include_once $_SERVER['DOCUMENT_ROOT'] .'/includes/admintopnav.html.php'; ?>
 
-		<h1>Change Employee Role</h1>
-		<form action="" method="post">
-			<div>
-				<label for="CompanyName">Company Name:</label>
-				<b id="CompanyName"><?php htmlout($CompanyName); ?></b>	
-			</div>
-			<div>				
-				<label for="UserIdentifier">User:</label>
-				<b id="UserIdentifier"><?php htmlout($UserIdentifier); ?></b>
-			</div>
-			<div>
-				<label for="CurrentCompanyPositionName">Current Role:</label>
-				<b id="CurrentCompanyPositionName"><?php htmlout($CurrentCompanyPositionName); ?></b>
-			</div>
-			<div>
-				<label for="PositionID">Set New Role:</label>
-				<select name="PositionID" id="PositionID">
-					<?php foreach($companyposition as $row): ?> 
-						<?php if($row['CompanyPositionName']==$CurrentCompanyPositionName):?>
-							<option selected="selected" 
-									value="<?php htmlout($row['PositionID']); ?>">
-									<?php htmlout($row['CompanyPositionName']);?>
-							</option>
-						<?php else : ?>
-							<option value="<?php htmlout($row['PositionID']); ?>">
-									<?php htmlout($row['CompanyPositionName']);?>
-							</option>
-						<?php endif;?>
-					<?php endforeach; ?>
-				</select>
-			</div>
-			<div>
-				<input type="hidden" name="CompanyID" value="<?php htmlout($CompanyID); ?>">
-				<input type="hidden" name="UserID" value="<?php htmlout($UserID); ?>">
-				<input type="submit" name="action" value="Confirm Role">
-				<input type="submit" name="action" value="Cancel">
-			</div>
-		</form>
+		<fieldset class="left"><legend>Change Employee Role</legend>
+			<form action="" method="post">
+				<div>
+					<label for="CompanyName">Company Name:</label>
+					<span><b id="CompanyName"><?php htmlout($CompanyName); ?></b></span>
+				</div>
+				<div>				
+					<label for="UserIdentifier">User:</label>
+					<span><b id="UserIdentifier"><?php htmlout($UserIdentifier); ?></b></span>
+				</div>
+				<div>
+					<label for="CurrentCompanyPositionName">Current Role:</label>
+					<span><b id="CurrentCompanyPositionName"><?php htmlout($CurrentCompanyPositionName); ?></b></span>
+				</div>
+				<div>
+					<label for="PositionID">Set New Role:</label>
+					<select name="PositionID" id="PositionID">
+						<?php foreach($companyposition as $row): ?> 
+							<?php if($row['CompanyPositionName'] == $CurrentCompanyPositionName):?>
+								<option selected="selected" 
+										value="<?php htmlout($row['PositionID']); ?>">
+										<?php htmlout($row['CompanyPositionName']);?>
+								</option>
+							<?php else : ?>
+								<option value="<?php htmlout($row['PositionID']); ?>">
+										<?php htmlout($row['CompanyPositionName']);?>
+								</option>
+							<?php endif;?>
+						<?php endforeach; ?>
+					</select>
+				</div>
+				<div>
+					<input type="hidden" name="CompanyID" value="<?php htmlout($CompanyID); ?>">
+					<input type="hidden" name="UserID" value="<?php htmlout($UserID); ?>">
+					<input type="submit" name="action" value="Confirm Role">
+					<input type="submit" name="action" value="Cancel">
+				</div>
+			</form>
+		</fieldset>
 		
 	<div class="left"><a href="..">Return to CMS home</a></div>
 	
