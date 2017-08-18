0004028babf8652bc419d60126b3a369b7fe1f9c
Nesnomis90 <Jusimonsen@gmail.com>
2017-08-18 16:13:07 +0200

    ...

--- user/index.php
+++ user/index.php
@@ -244,9 +244,9 @@ if(isSet($_POST['register']) AND $_POST['register'] == "Register Account"){
 		$description = "N/A";
 		$userinfo = $validatedLastname . ', ' . $validatedFirstname . ' - ' . $email;
 		if(isSet($_SESSION['LoggedInUserName'])){
-			$description = "An account for: " . $userinfo . " was registered by: " . $_SESSION['LoggedInUserName'];
+			$description = "An account for the user: " . $userinfo . " was registered by the user: " . $_SESSION['LoggedInUserName'];
 		} else {
-			$description = "An account was registered for " . $userinfo;
+			$description = "An account was registered for the user: " . $userinfo;
 		}
 		
 		if(isSet($_SESSION['lastUserID'])){
