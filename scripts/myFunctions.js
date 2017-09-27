f8cb4852c8686573503082f6d909741c00fbd285
Nesnomis90 <Jusimonsen@gmail.com>
2017-09-27 14:12:48 +0200

    ...

--- scripts/myFunctions.js
+++ scripts/myFunctions.js
@@ -1,7 +1,7 @@
 // The functions we will use to enhance our web pageX
 
 // Get a clock in text form
-function startTime(){
+/*function startTime(){
 	var today = new Date();
 	var dateWithDayName = today.toDateString();
 	var h = today.getHours();
@@ -18,7 +18,7 @@ function startTime(){
 
 	// Update every 0.5s
 	var t = setTimeout(startTime, 500);
-}
+}*/
 
 // add zero in front of numbers < 10 for our clock
 function checkTime(i){
@@ -26,4 +26,27 @@ function checkTime(i){
 		i = "0" + i
 	};
     return i;
+}
+
+function startTime(){
+	var today = new Date();
+	var dateWithDayName = today.toDateString();
+	var options = {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+		hour: '2-digit',
+		minute: '2-digit',
+		second: '2-digit'
+	};
+
+	// Define what ID we should update
+	var clockDiv = document.getElementById('Clock');
+
+	// Set new text (time)
+	clockDiv.innerHTML = today.toLocaleDateString("en-GB", options);
+
+	// Update every 0.5s
+	var t = setTimeout(startTime, 500);	
 }
